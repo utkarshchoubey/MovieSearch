0116402b56a70d80727fce1af4af420e1f10ffd4
unknown <utkarshchoubey629@gmail.com>
2018-05-20 15:08:42 +0530

    MovieDirectory

---
+++ app/src/main/java/com/gohool/moviedirectory/moviedirectory/Util/Constants.java
@@ -0,0 +1,13 @@
+package com.gohool.moviedirectory.moviedirectory.Util;
+
+
+
+public class Constants {
+
+
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    public static final String API_KEY = "&apikey=cdc4cef5";
+    public static final String URL_RIGHT = "&page=2";
+}
---
+++ app/src/test/java/com/gohool/moviedirectory/moviedirectory/Data/MovieRecyclerViewAdapterTest.java
@@ -0,0 +1,18 @@
+package com.gohool.moviedirectory.moviedirectory.Data;
+
+import org.junit.Test;
+
+public class MovieRecyclerViewAdapterTest {
+
+    @Test
+    public void onCreateViewHolder() {
+    }
+
+    @Test
+    public void onBindViewHolder() {
+    }
+
+    @Test
+    public void getItemCount() {
+    }
+}
